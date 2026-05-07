7cf0942430dd32383394989440c489deb5346cd7
chrisfromthepast <63051844+chrisfromthepast@users.noreply.github.com>
2026-05-07 09:18:44 -0400

    Rename RoomoveAudioProcessor to ArmorAudioProcessor

--- app/PluginProcessor.h
+++ app/PluginProcessor.h
@@ -1,35 +1,31 @@
 #pragma once
-
 #include <JuceHeader.h>
+#ifndef __TMS320C6X__
+    #include <RTNeural/RTNeural.h>
+#endif
 
-class RoomoveAudioProcessor : public juce::AudioProcessor
+class ArmorAudioProcessor : public juce::AudioProcessor 
 {
 public:
-    RoomoveAudioProcessor();
-    ~RoomoveAudioProcessor() override;
-
+    // ... standard JUCE methods ...
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
-    void releaseResources() override;
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
 
-    juce::AudioProcessorEditor* createEditor() override;
-    bool hasEditor() const override;
+private:
+    // 2. Hide the model instance from the TI compiler
+#ifndef __TMS320C6X__
+    std::unique_ptr<RTNeural::ModelT<float, 1, 1>> model;
+#endif
+    
+    // Lock-Free FIFO for the Mask (Safe for DSP)
+    juce::AbstractFifo fifo { 1024 }; 
+    float maskBuffer[1024]; 
+    float currentMask = 1.0f; // Default to pass-through
 
-    const juce::String getName() const override;
+    void runInference();
+    juce::AudioBuffer<float> sidechainBuffer;
+};
 
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
-
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const juce::String getProgramName (int index) override;
-    void changeProgramName (int index, const juce::String& newName) override;
-
-    void getStateInformation (juce::MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RoomoveAudioProcessor)
